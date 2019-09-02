a64ed3aebfc6f5ba910f14f7c251fe26e06dd1b4
Kartik <kartik.arora1214@gmail.com>
2019-09-02 19:18:10 +0530

    updated and tested awareness.js

--- awareness.js
+++ awareness.js
@@ -3,6 +3,7 @@ const config = require("./config.json");
 const fs = require('fs');
 const Store = require('data-store');
 const store = new Store({ path: 'crawled_stats.json' });
+const request = require("./await-request");
 
 let headers = {
     'Authorization': 'token ' + config.github_token,
@@ -23,6 +24,7 @@ async function getNext10RepositoriesFromPointer(afterPointer = "Y3Vyc29yOjMw") {
                     name
                     hasIssuesEnabled
                     homepageUrl
+                    nameWithOwner
                     descriptionHTML
                     url
                     stargazers {
@@ -79,8 +81,32 @@ async function next10() {
     var lastPointer = await getLastPointerFromLocalStore();
     var data = await getNext10RepositoriesFromPointer(lastPointer);
     await updateLocalStoreWithCrawlStatus(data.search.pageInfo.endCursor);
-    console.log(data);
+    // console.log(data);
+    // console.log(data.search.edges[0]);
+    // postIssueToGithub(data.search.edges[0].node);
     return data;
+}
+
+async function postIssueToGithub(repositoryNode) {
+    var headers = {
+        'Authorization': 'token ' + config.github_token,
+        "Accept": "application/vnd.github.symmetra-preview+json",
+        'User-Agent': 'flutterArsenal-cli'
+    };
+    var params = {
+        "title": `Add ${repositoryNode.name} to Flutter Arsenal`,
+        "body": `${repositoryNode.name} can be added to FlutterArsenal to help with reach, accessibility and ease-of-use. [Flutter Arsenal](https://flutterarsenal.com) is a directory that is being curated for Flutter libraries and tool. The best way to do is by creating an issue on [github](https://github.com/flutterarsenal/FlutterArsenal/issues/new/choose).
+        `
+    };
+    console.log(params);
+    
+    return await request({
+        method: 'post',
+        url: `https://api.github.com/repos/${repositoryNode.nameWithOwner}/issues`,
+        body: params,
+        headers: headers,
+        json: true
+    });
 }
 
 next10();
